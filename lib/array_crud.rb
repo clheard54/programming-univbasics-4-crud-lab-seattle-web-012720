b723be3238cab002d0b644176a44cb840efb6ab6
Clarion Heard <clahon54@gmail.com>
2019-12-03 21:32:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,11 +12,13 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(element)
+  array
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop
+  return array
 end
 
 def remove_element_from_start_of_array(array)
